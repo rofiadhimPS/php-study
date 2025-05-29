a677f277fbaafaee6c674528e76149fcdd1e3a35
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-29 19:32:26 +0700

    working with variable

---
+++ Fundamental PHP/Variable.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Adhim";
+$age = 23;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo "$age";
+echo "\n";
+
+// Variable variables
+$example = "adhim";
+$$example = "rofiadhim";
+
+echo "Example : ";
+echo $adhim;
+echo "\n";
