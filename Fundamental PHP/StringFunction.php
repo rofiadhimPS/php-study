f7b018117ea1bf4dfaef157aa9030b694636664a
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 12:45:44 +0700

    working with string function

---
+++ Fundamental PHP/StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10,9,8,7,6,5]));
+var_dump(explode(" ", "Muh Rofiadhim Rajab"));
+var_dump(strtolower("MUH ROFIADHIM RAJAB"));
+var_dump(strtoupper("Muh Rofiadhim Rajab"));
+var_dump(trim("     Muh     Rofiadhim    "));
+var_dump(substr("Muh Rofiadhim Rajab", 0, 6));
