a3bb81ec3da9d165a17f04986e53a7b72c1bddf9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 07:44:56 +0700

    ternary operator

---
+++ Fundamental PHP/TernaryOperator.php
@@ -0,0 +1,12 @@
+<?php
+
+$gender = "MAN";
+$hi = $gender == "MAN" ? "Hi, bro" : "Hi, nona";
+
+echo $hi . PHP_EOL;
+
+$gender = "WOMAN";
+$hi = $gender == "MAN" ? "Hi, bro" : "Hi, nona";
+
+echo $hi . PHP_EOL;
+
