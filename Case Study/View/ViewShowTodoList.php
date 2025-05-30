a7b7010191ba051db0b7f77692087c521971ca9f
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 15:49:05 +0700

    implementation view show todo list

--- Case Study/View/ViewShowTodoList.php
+++ Case Study/View/ViewShowTodoList.php
@@ -1,6 +1,32 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowTodoList()
 {
-    
+    while (true) {
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Add Todo" . PHP_EOL;
+        echo "2. Remove Todo" . PHP_EOL;
+        echo "x. Exit" . PHP_EOL;
+
+        $choice = input("Choice");
+
+        if($choice == "1"){
+            viewAddTodoList();
+        }else if ($choice == "2"){
+            viewRemoveTodoList();
+        }else if ($choice == "x"){
+            break;
+        }else {
+            echo "Choice not understood!" . PHP_EOL;
+        }
+    }
+
+    echo "See you again" . PHP_EOL;
 }
