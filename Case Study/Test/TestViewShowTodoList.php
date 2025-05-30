7c8bed5e38a7ae3b17fed1c70075ab69495af381
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 15:53:44 +0700

    testing view show todo list

---
+++ Case Study/Test/TestViewShowTodoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../View/ViewShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Muh");
+addTodoList("Rofiadhim");
+addTodoList("Rajab");
+addTodoList("Ryu");
+
+ViewShowTodoList();
--- Case Study/View/ViewShowTodoList.php
+++ Case Study/View/ViewShowTodoList.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "../Model/TodoList.php";
 require_once "../BusinessLogic/ShowTodoList.php";
 require_once "../View/ViewAddTodoList.php";
 require_once "../View/ViewRemoveTodoList.php";
