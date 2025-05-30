2add0f4d8d3e30da5146ec6e30051458f30e7863
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 07:58:27 +0700

    working with while loop

---
+++ Fundamental PHP/WhileLoop.php
@@ -0,0 +1,16 @@
+<?php
+
+$counter = 1;
+
+while($counter <= 10){
+    echo "Hello while loop " . $counter . PHP_EOL;
+    $counter++;
+}
+
+// AlternativeS= Syntax
+$counter = 1;
+
+while($counter <= 10){
+    echo "Hello while loop " . $counter . PHP_EOL;
+    $counter++;
+}
