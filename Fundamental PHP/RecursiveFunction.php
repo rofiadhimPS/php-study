20825cbc8e74ca244d367ff88cce6c4bb13a9db4
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 12:38:42 +0700

    working with recursive function

---
+++ Fundamental PHP/RecursiveFunction.php
@@ -0,0 +1,26 @@
+<?php
+
+function factorialLoop(int $value) : int
+{
+    $total = 1;
+
+    for ($i=1; $i<=$value; $i++) {
+        $total *= $i;
+    }
+
+    return $total;
+}
+
+var_dump(factorialLoop(5));
+
+// use recursive function
+function factorialRecursive(int $value): int 
+{
+    if($value == 1){
+        return 1;
+    }else {
+        return $value * factorialRecursive($value - 1);
+    }
+}
+
+var_dump(factorialRecursive(5));
