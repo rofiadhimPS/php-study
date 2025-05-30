4e5de73191290fe5d0c22ebc9834458608d545a8
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 16:13:09 +0700

    implementation view remove todo list

--- Case Study/View/ViewRemoveTodoList.php
+++ Case Study/View/ViewRemoveTodoList.php
@@ -1,6 +1,23 @@
 <?php
 
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
 function viewRemoveTodoList()
 {
+    echo "REMOVE TODO" . PHP_EOL;
+
+    $choice = input("Number (x for cancel)");
+
+    if($choice == "x"){
+        echo "Cancel remove todo" . PHP_EOL;
+    }else {
+        $success = removeTodoList($choice);
     
+        if($success) {
+            echo "Success delete todo selected number" . PHP_EOL;
+        } else {
+            echo "Failed to delete selected todo number" . PHP_EOL;
+        }
+    }
 }
