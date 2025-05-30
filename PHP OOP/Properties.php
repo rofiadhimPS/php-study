fe5e967cff688751cd4f6e0d12adce461cf54719
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 19:04:19 +0700

    working with properties

---
+++ PHP OOP/Properties.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "data/Person.php";
+
+$person = new Person();
+$person->name = "Adhim";
+$person->address = "Makassar";
+$person->country = "Indonesia";
+
+var_dump($person);
+
+echo "Name : $person->name" . PHP_EOL;
+echo "Address : $person->address" . PHP_EOL;
+echo "Country : $person->country" . PHP_EOL;
--- PHP OOP/data/Person.php
+++ PHP OOP/data/Person.php
@@ -2,5 +2,7 @@
 
 class Person
 {
-    
+    var string $name;
+    var ?string $address = null; // nullable properties
+    var string $country = "Indonesia";
 }
