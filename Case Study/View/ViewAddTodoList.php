ffdc969f200983c5884284ce16386d568862f6e1
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 16:00:45 +0700

    implementation view add todo list

--- Case Study/View/ViewAddTodoList.php
+++ Case Study/View/ViewAddTodoList.php
@@ -1,6 +1,20 @@
 <?php
 
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewAddTodoList()
 {
-    
+    echo "ADD TODO" . PHP_EOL;
+
+    $todo = input("Todo (x for cancel) : ");
+
+    if ($todo == "x") {
+        // batal
+    } else {
+        addTodoList($todo);
+    }
+
 }
+
