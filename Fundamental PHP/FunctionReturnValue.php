c5f12cdb93b88645937a322e9ad6eb9ebc03ce02
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 12:09:35 +0700

    working with function return value

---
+++ Fundamental PHP/FunctionReturnValue.php
@@ -0,0 +1,33 @@
+<?php
+
+function sum(int $first, int $second) : int
+{
+    $total = $first + $second;
+    return $total;
+}
+
+$result = sum(10,10);
+var_dump($result);
+
+function getFinalValue(int $value) : string
+{
+    if ($value >= 80){
+        return "A";
+    } else if ($value >= 70){
+        return "B";
+    } else if ($value >= 60){
+        return "C";
+    } else if ($value >= 50){
+        return "D";
+    } else {
+        return "E";
+    }
+}
+
+$score = getFinalValue(90);
+var_dump($score);
+
+
+
+    
+
