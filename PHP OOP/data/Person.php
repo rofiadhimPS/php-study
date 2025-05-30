7daa10a6b31ad076a3fe73e325cb8494f2f2c019
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-30 19:06:45 +0700

    adding function in class

--- PHP OOP/data/Person.php
+++ PHP OOP/data/Person.php
@@ -5,4 +5,9 @@ class Person
     var string $name;
     var ?string $address = null; // nullable properties
     var string $country = "Indonesia";
+
+    function sayHello(string $name)
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
 }
