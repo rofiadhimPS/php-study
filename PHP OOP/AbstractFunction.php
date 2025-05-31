fd47e3d6d4849ef72eb6dcb09d5e82bf625f948e
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-05-31 12:46:13 +0700

    working with abstract function

---
+++ PHP OOP/AbstractFunction.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "data/Animal.php";
+
+use Data\{Animal, Cat, Dog};
+
+$cat = new Cat();
+$cat->name = "Katy";
+$cat->run();
+
+$dog = new Dog();
+$dog->name = "Dogy";
+$dog->run();
+
