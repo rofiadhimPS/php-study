62e099f686a4c8e43d9c1c7432656df12ad15df4
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-06-02 12:52:26 +0700

    create view

---
+++ Case Study/View/TodolistView.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace View;
+
+class TodolistView
+{
+    function showTodolist(): void
+    {
+
+    }
+    
+    function addTodolist(): void
+    {
+
+    }
+
+    function removeTodolist(): void
+    {
+
+    }
+}
