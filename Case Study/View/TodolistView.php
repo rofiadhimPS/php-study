b4f75a9dd06d91d6e737870292dabf0002613140
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-06-02 18:56:17 +0700

    implementation view: show todo list

--- Case Study/View/TodolistView.php
+++ Case Study/View/TodolistView.php
@@ -2,11 +2,42 @@
 
 namespace View;
 
+use Helper\InputHelper;
+use Service\TodolistService;
+
 class TodolistView
 {
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+        $this->todolistService = $todolistService;
+    }
+
     function showTodolist(): void
     {
+        while (true) {
+            $this->todolistService->showTodolist();
 
+            echo "MENU" . PHP_EOL;
+            echo "1. Add Todo" . PHP_EOL;
+            echo "2. Remove Todo" . PHP_EOL;
+            echo "x. Exit" . PHP_EOL;
+
+            $choice = InputHelper::input("Choice");
+
+            if($choice == "1"){
+                $this->addTodolist();
+            }else if ($choice == "2"){
+                $this->removeTodolist();
+            }else if ($choice == "x"){
+                break;
+            }else {
+                echo "Choice not understood!" . PHP_EOL;
+            }
+    }
+
+    echo "See you again" . PHP_EOL;
     }
     
     function addTodolist(): void
