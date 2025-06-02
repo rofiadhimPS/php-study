cb0056bd92f563db6d0d187417b763ba3ef485d9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-06-02 10:21:46 +0700

    new string function

---
+++ PHP 8/NewStringFunction.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(str_contains("Muh Rofiadhim Rajab", "adhim"));
+var_dump(str_contains("Muh Rofiadhim Rajab", "Rajab"));
+var_dump(str_contains("Muh Rofiadhim Rajab", "Ryu"));
+
+var_dump(str_starts_with("Muh Rofiadhim Rajab", "adhim"));
+var_dump(str_starts_with("Muh Rofiadhim Rajab", "Rajab"));
+var_dump(str_starts_with("Muh Rofiadhim Rajab", "Ryu"));
+
+var_dump(str_ends_with("Muh Rofiadhim Rajab", "adhim"));
+var_dump(str_ends_with("Muh Rofiadhim Rajab", "Rajab"));
+var_dump(str_ends_with("Muh Rofiadhim Rajab", "Ryu"));
