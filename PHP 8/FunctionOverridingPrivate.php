2c795ee8c919d0ebefb62da931d6386f94f91cb6
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-06-02 09:57:09 +0700

    validation for function overriding

---
+++ PHP 8/FunctionOverridingPrivate.php
@@ -0,0 +1,16 @@
+<?php
+
+class Manager
+{
+    private function test(): void{
+
+    }
+}
+
+class VicePresident extends Manager
+{
+    public function test(string $name): string
+    {
+        return "VP";
+    }
+}
