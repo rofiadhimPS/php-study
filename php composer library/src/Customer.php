6f48557c88d8d9acdb7487cb7d3b5f8587dac261
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-06-10 08:44:31 +0700

    first commit

---
+++ php composer library/src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rofiadhim\ComposerLibrary;
+
+class Customer
+{
+    public function __construct(private string $name) 
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello, $name! My name is {$this->name}.";
+    }
+}
