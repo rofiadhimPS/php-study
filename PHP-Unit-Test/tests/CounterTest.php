60022ab7d35f6bb9434dd8d8ea0404592cf193eb
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-07-30 14:54:40 +0700

    add Depends attribute for testSecond

--- PHP-Unit-Test/tests/CounterTest.php
+++ PHP-Unit-Test/tests/CounterTest.php
@@ -3,6 +3,7 @@
 namespace App\Test;
 
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -23,14 +24,28 @@ class CounterTest extends TestCase
 
     }
 
-    #[Test]
-    public function increment()
+    /**
+     * @test
+     */
+    public function testIncrement()
     {
-            $counter = new Counter();
-            $counter->increment();
-            $this->assertEquals(1, $counter->getCount());
+        $counter = new Counter();
+        $counter->increment();
+        Assert::assertEquals(1, $counter->getCount());
+    }
 
-            $counter->increment();
-            $this->assertEquals(2, $counter->getCount());
+    public function testFirst(): Counter
+    {
+        $counter = new Counter();
+        $counter->increment();
+        Assert::assertEquals(1, $counter->getCount());
+        return $counter;
+    }
+
+    #[Depends('testFirst')]
+    public function testSecond(Counter $counter):void
+    {
+        $counter->increment();
+        Assert::assertEquals(2, $counter->getCount());
     }
 }
